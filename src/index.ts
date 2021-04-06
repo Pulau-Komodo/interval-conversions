f37d105e6176eb07cfec46bf04f6344070b84e54
Pulau_Komodo <komodohelico@gmail.com>
2021-04-06 01:34:49 +0000

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,162 @@
+const regexInterval = /^(?:(-)?(\d*\.?\d+) ?y(?:ears?)?\s?)?(?:(-)?(\d*\.?\d+) ?mo(?:nths?)?\s?)?(?:(-)?(\d*\.?\d+) ?w(?:eeks?)?\s?)?(?:(-)?(\d*\.?\d+) ?d(?:ays?)?\s?)?(?:(-)?(\d*\.?\d+) ?h(?:ours?)?\s?)?(?:(-)?(\d*\.?\d+) ?m(?:in(?:ute)?s?)?\s?)?(?:(-)?(\d*\.?\d+) ?s(?:ec(?:ond)?s?)?\s?)?$/
+
+const SECOND = 1000;
+const MINUTE = 60 * SECOND;
+const HOUR = 60 * MINUTE;
+const DAY = 24 * HOUR;
+const WEEK = 7 * DAY;
+
+/** Parses a user string like "1y 2mo 3w 4d 5h 6m 7s" and returns the duration in ms. If there is a component of variable duration (year or month), it will use the supplied date or now as a starting point. Returns undefined if parsing failed. */
+export const parseInterval = (text: string, startDate?: Date): number | undefined => {
+	text = text.trim();
+	if (!text)
+		return; // Empty input
+	const result = regexInterval.exec(text);
+	if (!result)
+		return; // Didn't look like a duration
+	let sign = 1;
+	const parseNumber = (minus: string, number: string): number => {
+		sign *= minus ? -1 : 1;
+		return number ? sign * Number(number) : 0;
+	};
+	const timeElements = [];
+	for (let i = 1; i < 15; i += 2) {
+		timeElements.push(parseNumber(result[i], result[i + 1]));
+	}
+	const [y, mo, w, d, h, m, s] = timeElements;
+	const invariable = w * WEEK + d * DAY + h * HOUR + m * MINUTE + s * SECOND;
+	if (y === 0 && mo === 0) { // No variable elements
+		return invariable;
+	}
+	const date = startDate ? new Date(startDate) : new Date();
+	const startingPoint = date.getTime();
+	date.setUTCFullYear(
+		date.getUTCFullYear() + y,
+		date.getUTCMonth() + mo,
+	);
+	return date.getTime() + invariable - startingPoint;
+};
+
+/** Stringifies a ms interval like "1 day, 5 hours and 20 minutes". It says seconds only if the interval is under 10 minutes. */
+ export const stringifyInterval = (interval: number): string => {
+	interval = Math.round(interval / 1000);
+	if (interval < 0)
+		interval = -1*interval;
+	
+	let outputElements = 0;
+	// Inserts a comma or "and" as appropriate
+	const joiner = function() {
+		outputElements--;
+		if (outputElements > 1)
+			return ", ";
+		if (outputElements === 1)
+			return " and ";
+		return "";
+	};
+
+	let text = "";
+	const under10Minutes = interval < 10*60;
+	
+	let days = Math.floor(interval / (24*60*60));
+	interval -= days * 24*60*60;
+	let hours = Math.floor(interval / (60*60));
+	interval -= hours * 60*60;
+	let minutes = under10Minutes ? Math.floor(interval / 60) : Math.round(interval / 60);
+	const seconds = interval - minutes * 60;
+
+	// If minutes got rounded up, make sure it propagates to the other units
+	if (minutes === 60) {
+		minutes = 0;
+		hours++;
+		if (hours === 24) {
+			hours = 0;
+			days++;
+		}
+	}
+	
+	// Calculate number of text elements to be joined with comma/and
+	if (days > 0) {outputElements++;}
+	if (hours > 0) {outputElements++;}
+	if (minutes > 0) {outputElements++;}
+	if (under10Minutes && seconds > 0) {outputElements++;}
+	
+	if (days > 1) {
+		text += `${days} days${joiner()}`;
+	} else if (days == 1) {
+		text += `1 day${joiner()}`;
+	}
+	if (hours > 1) {
+		text += `${hours} hours${joiner()}`;
+	} else if (hours == 1) {
+		text += `1 hour${joiner()}`;
+	}
+	if (minutes > 1) {
+		text += `${minutes} minutes${joiner()}`;
+	} else if (minutes == 1) {
+		text += `1 minute${joiner()}`;
+	}
+	if (under10Minutes) { // If it's less than 10 minutes, say seconds too
+		if (seconds === 1) {
+			text += `1 second`;
+		} else {
+			text += `${seconds} seconds`;
+		}
+	}
+	
+	return text;
+};
+
+const MINUTE_S = MINUTE / 1000;
+const HOUR_S = HOUR / 1000;
+const DAY_S  = DAY / 1000;
+const YEAR_S = 365.25 * DAY_S;
+const YEAR_S_LOWER = 365 * DAY_S;
+const YEAR_S_UPPER = 366 * DAY_S;
+const MONTH_S = YEAR_S / 12;
+const MONTH_S_LOWER = 28 * DAY_S;
+const MONTH_S_UPPER = 31 * DAY_S;
+
+/** Stringifies a ms interval to the smallest number of the largest suitable unit (between minutes, hours days, months and years) that contains it, like "5 days", "5 months" or "~5 years". To be used with "within" or "under" etc. A tilde is added if using the lower or upper bound of a month or year makes a difference. If atLeast is true, it will round down instead, and it should be used with "at least" etc. */
+export const stringifyIntervalShort = (interval: number, atLeast = false): string => {
+	interval = interval / 1000
+	const round = atLeast ? Math.floor : Math.ceil;
+	if (interval < 0)
+		interval = -1*interval;
+
+	if (interval < HOUR_S) {
+		const minutes = round(interval / MINUTE_S);
+		if (minutes !== 60) {
+			const s = minutes === 1 ? "" : "s";
+			return `${minutes} minute${s}`;
+		}
+	}
+
+	if (interval < DAY_S) {
+		const hours = round(interval / HOUR_S);
+		if (hours !== 24) {
+			const s = hours === 1 ? "" : "s";
+			return `${hours} hour${s}`;
+		}
+	}
+
+	if (interval < MONTH_S_UPPER) {
+		const days = round(interval / DAY_S);
+		const s = days === 1 ? "" : "s";
+		return `${days} day${s}`;
+	}
+	
+	if (interval < YEAR_S) {
+		const months = round(interval / MONTH_S);
+		if (months !== 12) {
+			const s = months === 1 ? "" : "s";
+			const tilde = round(interval / MONTH_S_LOWER) === round(interval / MONTH_S_UPPER) ? "" : "~";
+			return `${tilde}${months} month${s}`;
+		}
+	}
+
+	const years = round(interval / YEAR_S);
+	const s = years === 1 ? "" : "s";
+	const tilde = round(interval / YEAR_S_LOWER) === round(interval / YEAR_S_UPPER) ? "" : "~";
+	return `${tilde}${years} year${s}`;
+};
+// Tildes could be applied more intelligently. There is no risk of 4 leap years or 5 28-day months in a row.
---
+++ src/test/tests.ts
@@ -0,0 +1,76 @@
+import { stringifyInterval, stringifyIntervalShort, parseInterval } from "../index.js";
+
+const stringifyInput = (element: unknown): string => {
+	if (typeof element === "string") {
+		return `"${element}"`;
+	} else if (typeof element === "number") {
+		return element.toString();
+	} else if (Array.isArray(element)) {
+		return `[${element.map(stringifyInput)}]`;
+	} else if (typeof element === "object") {
+		return "[some object]";
+	} else {
+		const string = `${element}`;
+		if (string.length > 30) {
+			return `${string.slice(0, 30)}…`;
+		}
+		return string;
+	}
+}
+
+const test = <I extends ReadonlyArray<unknown>, O>(func: (...arg0: I) => O, input: I, expected: O | Error) => {
+	const inputText = input.map(stringifyInput);
+	try {
+		const result = func(...input);
+		const resultText = typeof result === "string" ? `"${result}"` : `${result}`;
+		if (result === expected) {
+			console.log(`✔\ufe0f ${func.name}(${inputText}) successfully returned ${resultText}`);
+		} else {
+			const expectedText =
+				expected instanceof Error ? `throwing ${expected}` :
+				typeof expected === "string" ? `"${expected}"` :
+				`${expected}`
+			console.error(`❌ ${func.name}(${inputText}) returned ${resultText} instead of ${expectedText}`);
+		}
+	} catch (error) {
+		if (
+			expected instanceof Error && error instanceof Error
+			&& expected.constructor === error.constructor
+			&& expected.message === error.message
+			&& expected.name === expected.name
+		) {
+			console.log(`✔\ufe0f ${func.name}(${inputText}) successfully threw ${expected.name}`);
+		} else {
+			const expectedText =
+				expected instanceof Error ? `throwing ${expected}` :
+				typeof expected === "string" ? `returning "${expected}"` :
+				`returning ${expected}`;
+			console.error(`❌ ${func.name}(${inputText}) threw ${error} instead of instead of ${expectedText}`);
+			console.error(error);
+		}
+	}
+};
+
+test(parseInterval, ["5d"], 432000000);
+test(parseInterval, ["100y -10s", new Date("1900")], 3155673590000);
+test(parseInterval, ["5a"], undefined);
+test(parseInterval, [".5s"], 500);
+test(parseInterval, [" 1 year 4 mo -4 h -30.5 min 100s ", new Date("1900")], 41891530000);
+test(stringifyInterval, [500000], "8 minutes and 20 seconds");
+test(stringifyInterval, [-5000000], "1 hour and 23 minutes");
+test(stringifyInterval, [50000000], "13 hours and 53 minutes");
+test(stringifyInterval, [-500000000], "5 days, 18 hours and 53 minutes");
+test(stringifyInterval, [5000000000], "57 days, 20 hours and 53 minutes");
+test(stringifyInterval, [-50000000000], "578 days, 16 hours and 53 minutes");
+test(stringifyIntervalShort, [500000], "9 minutes")
+test(stringifyIntervalShort, [-5000000], "2 hours");
+test(stringifyIntervalShort, [50000000], "14 hours")
+test(stringifyIntervalShort, [-500000000], "6 days");
+test(stringifyIntervalShort, [5000000000], "~2 months")
+test(stringifyIntervalShort, [-50000000000], "2 years");
+test(stringifyIntervalShort, [500000, true], "8 minutes")
+test(stringifyIntervalShort, [-5000000, true], "1 hour");
+test(stringifyIntervalShort, [50000000, true], "13 hours")
+test(stringifyIntervalShort, [-500000000, true], "5 days");
+test(stringifyIntervalShort, [5000000000, true], "~1 month")
+test(stringifyIntervalShort, [-50000000000, true], "1 year");
