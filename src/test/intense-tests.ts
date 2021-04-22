8c962ebb2ff278a4de0233543a916db960ae0bd4
Pulau_Komodo <komodohelico@gmail.com>
2021-04-22 01:46:19 +0000

    Optimized the intense test The test loop itself was using significant processing time

--- src/test/intense-tests.ts
+++ src/test/intense-tests.ts
@@ -1,4 +1,4 @@
-import { stringifyInterval, StringifyThresholds } from "../stringifyInterval";
+import { stringifyInterval, StringifyThresholds } from "../stringifyInterval.js";
 
 const oneUnit: StringifyThresholds = {
 	years: [0, Infinity],
@@ -14,10 +14,11 @@ const stepSize = -1000;
 
 let lastOutcome = "";
 let i = 0;
-while (!lastOutcome.includes("2 years")) {
+while (true) {
 	const outcome = stringifyInterval(i, { startDate: new Date("2000"), thresholds: oneUnit });
 	if (outcome !== lastOutcome) {
 		console.log(`${outcome} from ${i}ms`);
+		if (outcome.includes("2 years")) break;
 		lastOutcome = outcome;
 	}
 	i += stepSize;
